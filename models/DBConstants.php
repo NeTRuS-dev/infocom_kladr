1b45122e980967bf0915341bc11c3e77babccc72
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-10 14:27:37 +0300

    name constants

---
+++ models/DBConstants.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\models;
+
+
+class DBConstants
+{
+    const ALTNAMES='ALTNAMES.dbf';
+    const DOMA='DOMA.dbf';
+    const KLADR='KLADR.dbf';
+    const NAMEMAP='NAMEMAP.dbf';
+    const SOCRBASE='SOCRBASE.dbf';
+    const STREET='STREET.dbf';
+}
